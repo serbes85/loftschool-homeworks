fa73e86ac8a7a42acc8d10c6ee6ba7c0ae16209c
serbes85 <beskrovnyisergei@mail.ru>
2020-07-14 10:02:17 +0300

    Homework has done

--- src/UseStateHook/UseStateHook.js
+++ src/UseStateHook/UseStateHook.js
@@ -22,4 +22,42 @@ import React, { useState } from "react";
     https://reactjs.org/docs/hooks-reference.html#usereducer
 */
 
-export const Form = () => {};
+export const Form = () => {
+  const [email, changeEmail] = useState("");
+  const [password, changePassword] = useState("");
+  let [success, makeSuccess] = useState(false);
+
+  function onChangeEmail(e) {
+    changeEmail(e.target.value);
+  }
+  function onChangePassword() {
+    changePassword(e.target.value);
+  }
+  function onClick(e) {
+    if (email !== "" && password !== "") {
+      makeSuccess(true);
+    } else {
+      ("ошибка");
+    }
+  }
+  return (
+    <React.Fragment>
+      <input
+        className="email"
+        name="email"
+        data-testid="input-email"
+        value={email}
+        onChange={onChangeEmail}
+      />
+      <input
+        className="password"
+        name="password"
+        data-testid="input-password"
+        value={email}
+        onChange={onChangePassword}
+      />
+      <button data-testid="submit" onClick={onClick} />
+      {success && <div data-testid="success-message">Вы вошли</div>}
+    </React.Fragment>
+  );
+};
